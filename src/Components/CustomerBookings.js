cc617ebe62cd9f8ea880b2f6369d73685a96a4b5
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-22 16:03:15 +0100

    displayes bookings list for each customer

--- src/Components/CustomerBookings.js
+++ src/Components/CustomerBookings.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 const CustomerBookings = (props) => {
+  if (!props.customer) return null;
 console.log(props.customer)
 const bookings = props.customer.bookings.map((booking, index) => {
   return (<div key={index}>
     Customer: {booking.customer.name} <br />
     Table: {booking.restaurantTable.number} <br />
-    Time: {booking.time} PM
+    Time: {booking.time} PM <br />
+    Receipt:
     </div>)
 })
 return (
   <div>
-    {bookings}
-  </div>
+  {bookings} </div>
 )
 
 }
