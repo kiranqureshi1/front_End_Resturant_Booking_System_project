1d89be0d3d9317a1463937b377a7d16c604f0795
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-23 17:45:30 +0100

    add Recipt container added

--- src/containers/AddCustomerReceiptContainer.js
+++ src/containers/AddCustomerReceiptContainer.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import Request from '../../helpers/request';
 
+import AddCustomersReceipt from '../Components/AddCustomersReceipt';
+
+const  AddCustomersReceiptContainer = (props) => {
+
+  function handleReceiptPost(receipt){
+  const request = new Request();
+  request.post('/bookings', receipt).then(() => {
+    window.location = '/'
+  })
+
+  return (
+    <AddCustomersReceipt handleReceiptPost = {this.handleReceiptPost}/>
+  )
+
+}
+export default AddCustomersReceiptContainer;
