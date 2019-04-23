7d188056a060cdbb5f9792026488d297dc39661d
Martin <sileshs@gmail.com>
2019-04-23 11:45:38 +0100

    resolving tablelist conflict

--- src/Components/TableListComponent.js
+++ src/Components/TableListComponent.js
@@ -1,9 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import {Link} from 'react-router-dom'
 
-=======
->>>>>>> cc617ebe62cd9f8ea880b2f6369d73685a96a4b5
 import "./css/TableListComponent.css";
 
 
@@ -16,7 +13,6 @@ const TableList = (props) => {
       </div>
 
     } else {
-<<<<<<< HEAD
       return <Link to={`/newbooking/${table.tableNumber}/${props.date}`} key={index}><div className="table" key={index}>
       {table.tableNumber}
     </div></Link>
@@ -24,14 +20,6 @@ const TableList = (props) => {
     })
 
 
-=======
-      return <div className="table" key={index} onClick={props.handleNewBooking}>
-      {table.tableNumber}
-      </div>
-    }
-    })
-
->>>>>>> cc617ebe62cd9f8ea880b2f6369d73685a96a4b5
   return(
     <div className="table-list">
       {tables}
