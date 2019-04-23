ba6f6197e6059ca1ef6b306e89b0e3dbc82d97b6
kiranqureshi1 <kiranqureshi@outlook.com>
2019-04-23 11:12:38 +0100

    customer total spent file done and displays

--- src/Components/CustomerTotalAmountSpent.js
+++ src/Components/CustomerTotalAmountSpent.js
@@ -1,1 +1,21 @@
+import React from "react";
 
+const CustomerTotalAmountSpent = (props) => {
+  if (!props.customer) return null;
+
+  let total = 0
+  for (const booking of props.customer.bookings){
+    total += booking.receipt;
+  }
+  console.log(total)
+  return total;
+
+  return (
+    <div>
+     {total}
+    </div>
+  )
+
+}
+
+export default CustomerTotalAmountSpent;
